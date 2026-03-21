7e17d2319453de5efaf046b160240b29462b7192
Anderson Oliveira <anderson.programador.si@gmail.com>
2026-03-21 10:52:56 -0500

    --refactor: Criando template para páginas

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,7 @@
 import './styles/theme.css';
 import './styles/global.css';
-
-import { Container } from './components/Container';
-import { Logo } from './components/Logo';
-import { Menu } from './components/Menu';
-import { CountDown } from './components/CountDown';
-import { DefaultInput } from './components/DefaultInput';
-import { Cycles } from './components/Cycles';
-import { DefaultButton } from './components/DefaultButton';
-import { PlayCircleIcon, StopCircleIcon } from 'lucide-react';
-import { Footer } from './components/Footer';
+import { Home } from './page/Home';
 
 export function App() {
-  return (
-    <>
-      <Container>
-        <Logo />
-      </Container>
-      <Container>
-        <Menu />
-      </Container>
-      <Container>
-        <CountDown />
-      </Container>
-      <Container>
-        <form action='' className='form'>
-          <div className='formRow'>
-            <DefaultInput
-              type='text'
-              id='taskInput'
-              labelText='Task'
-              placeholder='Digite algo'
-            />
-          </div>
-
-          <div className='formRow'>
-            <p>Lorem ipsum dolor sit amet.</p>
-          </div>
-
-          <div className='formRow'>
-            <Cycles />
-          </div>
-
-          <div className='formRow'>
-            <DefaultButton icon={<PlayCircleIcon />} />
-          </div>
-        </form>
-      </Container>
-      <Container>
-        {' '}
-        <Footer />{' '}
-      </Container>
-    </>
-  );
+  return <Home />;
 }
---
+++ src/components/MainForm/index.tsx
@@ -0,0 +1,31 @@
+import { PlayCircleIcon } from 'lucide-react';
+import { Cycles } from '../Cycles';
+import { DefaultInput } from '../DefaultInput';
+import { DefaultButton } from '../DefaultButton';
+
+export function MainForm() {
+  return (
+    <form action='' className='form'>
+      <div className='formRow'>
+        <DefaultInput
+          type='text'
+          id='taskInput'
+          labelText='Task'
+          placeholder='Digite algo'
+        />
+      </div>
+
+      <div className='formRow'>
+        <p>Lorem ipsum dolor sit amet.</p>
+      </div>
+
+      <div className='formRow'>
+        <Cycles />
+      </div>
+
+      <div className='formRow'>
+        <DefaultButton icon={<PlayCircleIcon />} />
+      </div>
+    </form>
+  );
+}
