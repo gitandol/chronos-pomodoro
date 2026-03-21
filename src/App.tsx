7fc7838d79e8e66eddb1bd88ac2339df47506f5d
Anderson Oliveira <anderson.programador.si@gmail.com>
2026-03-21 12:42:55 -0500

    --feat: add estado global com useState e props

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
 import './styles/theme.css';
 import './styles/global.css';
 import { Home } from './page/Home';
+import type { TaskStateModel } from './models/TaskStateModel';
+import { useState } from 'react';
+
+const initialState: TaskStateModel = {
+  tasks: [],
+  secondsRemaining: 0,
+  formattedSecondsRemaining: '00:00',
+  activeTask: null,
+  currentCycle: 0,
+  config: {
+    workTime: 25,
+    shortBreakTime: 5,
+    longBreakTime: 15,
+  },
+};
 
 export function App() {
-  return <Home />;
+  const [state, setState] = useState(initialState);
+
+  return <Home state={state} setState={setState} />;
 }
